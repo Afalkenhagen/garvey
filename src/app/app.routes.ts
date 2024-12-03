cbbf6ad20aef55b220051201be01be8dd08beea1
afalkenhagen <ahau_buluk@protonmail.ch>
2024-12-03 20:03:56 -0300

    feat new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: HomeComponent},
+    { path: "nav", component: NavbarComponent},
+    { path: "footer", component: FooterComponent}
+];
