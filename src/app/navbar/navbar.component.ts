ed6c38bff2e0136d9b3e25ff90d90700c85ddfd3
afalkenhagen <ahau_buluk@protonmail.ch>
2025-01-16 20:29:43 -0300

    chore navbar feat

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 
 @Component({
@@ -10,6 +10,44 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
   isMenuOpen = false;
+  lastScrollTop = 0;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const navbar = document.getElementById('navbar');
+    if (navbar) {
+      const scrollTop = window.scrollY;
+      if (scrollTop > this.lastScrollTop) {
+        // Scroll down
+        navbar.classList.add('-translate-y-full');
+        navbar.classList.remove('translate-y-0');
+      } else {
+        // Scroll up
+        navbar.classList.remove('-translate-y-full');
+        navbar.classList.add('translate-y-0');
+      }
+      this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+    }
+  }
+
+  //VERSION SIEMPRE VISIBLE
+  // @HostListener('window:scroll', [])
+  // onWindowScroll() {
+  //   const navbar = document.getElementById('navbar');
+  //   if (navbar) {
+  //     const scrollTop = window.scrollY;
+  //     if (scrollTop > this.lastScrollTop) {
+  //       // Scroll down
+  //       navbar.classList.remove('-translate-y-full');
+  //       navbar.classList.add('translate-y-0');
+  //     } else {
+  //       // Scroll up
+  //       navbar.classList.remove('-translate-y-full');
+  //       navbar.classList.add('translate-y-0');
+  //     }
+  //     this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+  //   }
+  // }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
