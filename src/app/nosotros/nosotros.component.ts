b6235ebe5cf35f1310059dd5a83a6be24da65ecc
afalkenhagen <ahau_buluk@protonmail.ch>
2025-02-22 09:39:03 -0300

    feat: video headers

--- src/app/nosotros/nosotros.component.ts
+++ src/app/nosotros/nosotros.component.ts
@@ -3,10 +3,11 @@ import { FactsComponent } from '../facts/facts.component';
 import { NgOptimizedImage } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { AboutComponent } from '../about/about.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nosotros',
-  imports: [FactsComponent, NgOptimizedImage, MatIconModule, AboutComponent],
+  imports: [FactsComponent, NgOptimizedImage, MatIconModule, AboutComponent, RouterLink],
   templateUrl: './nosotros.component.html',
   styleUrl: './nosotros.component.scss'
 })
--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-productos',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, RouterLink],
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.scss'
 })
