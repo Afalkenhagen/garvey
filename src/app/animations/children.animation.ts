eadd9765a1c50eccd3810824e08005cead1ba7ad
afalkenhagen <ahau_buluk@protonmail.ch>
2025-06-10 19:52:48 -0300

    style: hero component animations

---
+++ src/app/animations/children.animation.ts
@@ -0,0 +1,45 @@
+import { trigger,style,animate,transition,query,animateChild,} from '@angular/animations';
+
+export const Container = [
+  trigger('container', [
+    transition(':enter, :leave', [
+      query('@*', animateChild(), { optional: true }),
+    ]),
+  ]),
+];
+
+export const EnterExitLeft = [
+  trigger('enterExitLeft', [
+    transition(':enter', [
+      style({ opacity: 0, transform: 'translateX(-200px)' }),
+      animate(
+        '500ms ease-out',
+        style({ opacity: 1, transform: 'translateX(0)' })
+      ),
+    ]),
+    transition(':leave', [
+      animate(
+        '500ms ease-out',
+        style({ opacity: 0, transform: 'translateX(-200px)' })
+      ),
+    ]),
+  ]),
+];
+
+export const EnterExitRight = [
+  trigger('enterExitRight', [
+    transition(':enter', [
+      style({ opacity: 0, transform: 'translateX(200px)' }),
+      animate(
+        '500ms ease-in',
+        style({ opacity: 1, transform: 'translateX(0)' })
+      ),
+    ]),
+    transition(':leave', [
+      animate(
+        '500ms ease-in',
+        style({ opacity: 0, transform: 'translateX(200px)' })
+      ),
+    ]),
+  ]),
+];
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common'
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Container, EnterExitLeft, EnterExitRight } from '../animations/children.animation';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-hero',
-  imports: [NgOptimizedImage, RouterLink, RouterLinkActive],
+  imports: [NgOptimizedImage, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.scss'
+  styleUrl: './hero.component.scss',
+  animations: [Container,EnterExitLeft,EnterExitRight]
 })
 export class HeroComponent {
-
+  isDisplayed = false;
+  ngOnInit() {
+    // Simula una condición para mostrar el contenido después de un tiempo
+    setTimeout(() => {
+      this.isDisplayed = true;
+    }, 500); // Muestra el contenido después de 0.5 segundos
+  }
 }
