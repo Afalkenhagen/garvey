12513ec8cd1b2582f1b2635e1c93543442121f49
afalkenhagen <ahau_buluk@protonmail.ch>
2025-08-06 20:42:08 -0300

    style: update

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ObserveVisibilityDirective } from '../../directives/observe-visibility.directive';
 
 @Component({
   selector: 'app-contacto',
-  imports: [RouterLink],
+  imports: [RouterLink, ObserveVisibilityDirective],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.scss'
 })
--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CategoryComponent } from '../category/category.component';
+import { ObserveVisibilityDirective } from '../../directives/observe-visibility.directive';
 
 
 @Component({
   selector: 'app-productos',
-  imports: [NgOptimizedImage, RouterLink, CategoryComponent, RouterLinkActive],
+  imports: [NgOptimizedImage, RouterLink, CategoryComponent, RouterLinkActive, ObserveVisibilityDirective],
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.scss'
 })
