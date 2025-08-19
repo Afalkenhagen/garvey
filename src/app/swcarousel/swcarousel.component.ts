364bd6ee40ef9c003621c606f032c5cfc01e181e
afalkenhagen <ahau_buluk@protonmail.ch>
2025-08-19 19:58:06 -0300

    style: carousel fix

--- src/app/swcarousel/swcarousel.component.ts
+++ src/app/swcarousel/swcarousel.component.ts
@@ -1,10 +1,11 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { register } from 'swiper/element/bundle';
 register();
 
 @Component({
   selector: 'app-swcarousel',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './swcarousel.component.html',
   styleUrl: './swcarousel.component.scss'
