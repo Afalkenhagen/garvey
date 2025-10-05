359ade0f4894d687f04483103dbce125fb8567bc
afalkenhagen <ahau_buluk@protonmail.ch>
2025-10-05 20:12:54 -0300

    feat: Nosotros component update

--- src/app/nosotros/nosotros.component.ts
+++ src/app/nosotros/nosotros.component.ts
@@ -4,10 +4,11 @@ import { NgOptimizedImage } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { AboutComponent } from '../about/about.component';
 import { RouterLink } from '@angular/router';
+import { ObserveVisibilityDirective } from '../../directives/observe-visibility.directive';
 
 @Component({
   selector: 'app-nosotros',
-  imports: [FactsComponent, NgOptimizedImage, MatIconModule, AboutComponent, RouterLink],
+  imports: [FactsComponent, NgOptimizedImage, MatIconModule, AboutComponent, RouterLink, ObserveVisibilityDirective],
   templateUrl: './nosotros.component.html',
   styleUrl: './nosotros.component.scss'
 })
