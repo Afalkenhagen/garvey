2793aa44f817e2380d7de5382cad5e8e2cced03c
afalkenhagen <ahau_buluk@protonmail.ch>
2025-10-05 19:15:47 -0300

    feat: instagram banner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,11 +6,12 @@ import { FactsComponent } from '../facts/facts.component';
 import { AboutComponent } from '../about/about.component';
 import { FeatureComponent } from "../feature/feature.component";
 import { SwcarouselComponent } from '../swcarousel/swcarousel.component';
+import { ObserveVisibilityDirective } from '../../directives/observe-visibility.directive';
 
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, HeroComponent, FactsComponent, AboutComponent, FeatureComponent, SwcarouselComponent],
+  imports: [CommonModule, HeroComponent, FactsComponent, AboutComponent, FeatureComponent, SwcarouselComponent, ObserveVisibilityDirective],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
